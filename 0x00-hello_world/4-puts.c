1c8518dbd2c88f3faa7c5374d09c05f72d4e96c7
MCJKiseki <kisekicarol@gmail.com>
2022-03-11 07:29:48 -0800

    c4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- *  * main - Entry point
- *  * Return: Always 0 (Success)
- *  */
+ *  * main - display a message
+ *   * Return: display the message
+ *    */
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+		char comment[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(comment);
+
+				return (0);
 }
