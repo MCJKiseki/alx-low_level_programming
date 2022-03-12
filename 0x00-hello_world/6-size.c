40832efb7b99c67877d89f3eeb7794a99c7ebc54
MCJKiseki <kisekicarol@gmail.com>
2022-03-12 16:55:21 +0300

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
-/**
+#include <stdio.h>b
+/*
  * main - Entry point
- *  
- * Return: Always 0 (Success)
- */
+ * Description
+ * Return: Always 0 (Success) 
+*/
 
 int main(void)
-
 {
 	int i;
 	long int i2;
